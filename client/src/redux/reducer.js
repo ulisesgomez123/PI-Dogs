b4d5a13b5b2a78ff3c996271458a67ae635489da
ulises Gomez <ulisescap123@gmail.com>
2022-11-07 19:54:19 -0300

    hh

---
+++ client/src/redux/reducer.js
@@ -0,0 +1,30 @@
+let initialState = {
+   dogsLoaded: []
+  };
+   
+  export default function rootReducer(state = initialState, action) {
+    switch (action.type) {
+      case "GET_DOGS":
+      return {
+        ...state, 
+        dogs: action.payload
+      }
+      case "GET_MOVIES":
+        return {
+            ...state,
+            moviesLoaded: action.payload.Search
+        }
+        case "REMOVE_MOVIE_FAVORITE":
+            return {
+                ...state,
+                moviesFavorites: state.moviesFavorites.filter(e => 
+                    e.id !== action.payload )
+            }
+        case "GET_MOVIE_DETAIL":
+            return {
+                ...state,
+                movieDetail: action.payload
+            }
+      default: return state;
+    }
+  }
